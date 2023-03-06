b7a9dd0e6eee40be8a352a4664e4e4b15534fa6d
Vyanei Salazar <manchas_0207@hotmail.com>
2023-03-05 23:46:23 -0600

    Solucion ejercicio semana 1

---
+++ ANAGRAMA.py
@@ -0,0 +1,43 @@
+"""
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+"""
+
+def anagrama(palabra1, palabra2):
+    # Convertimos ambas a minúsculas
+    palabra1 = palabra1.lower()
+    palabra2 = palabra2.lower()
+    # Luego convertimos las cadenas a arreglos, porque vamos a ordenarlas pues es más sencillo ordenar un arreglo
+    palabra1_arreglo = list(palabra1)
+    palabra2_arreglo = list(palabra2)
+    # Las ordenamos
+    palabra1_arreglo.sort()
+    palabra2_arreglo.sort()
+    # Convertir de vuelta a cadena
+    palabra1_ordenada = "".join(palabra1_arreglo)
+    palabra2_ordenada = "".join(palabra2_arreglo)
+    # Y finalmente comprobar si son iguales
+    return palabra1_ordenada == palabra2_ordenada
+
+"""
+cadena1 = "Nacionalista"
+cadena2 = "Altisonancia"
+es_anagrama = anagrama(cadena1, cadena2)
+if es_anagrama:
+    print(f"{cadena1} es anagrama de {cadena2}")
+else:
+    print(f"{cadena1} NO es anagrama de {cadena2}")
+"""
+
+# Solicitar al usuario las cadenas
+cadena1 = input("Ingresa una cadena: ")
+cadena2 = input("Ingresa otra cadena: ")
+es_anagrama = anagrama(cadena1, cadena2)
+if es_anagrama:
+    print(f"{cadena1} es anagrama de {cadena2}")
+else:
+    print(f"{cadena1} NO es anagrama de {cadena2}")
